601737f1b85dd100118cd7682ef59abe65725149
root <root@pythonwork.(none)>
2013-03-25 00:13:15 +0000

    DONE

--- challenge1.py
+++ challenge1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-#Challenge:  Write a script that builds three 512 MB Cloud Servers that following a similar naming convention. (ie., web1, web2, web3) and returns the IP and login credentials for each server. Use any image you want. Worth 1 point
+#Challenge:  Write a script that builds three 512 MB Cloud Servers that following a similar naming convention. 
+#(ie., web1, web2, web3) and returns the IP and login credentials for each server. Use any image you want. 
+#Worth 1 point
 
 
 import pyrax
@@ -34,7 +36,7 @@ for server in range(len(servreq)):
         server_name = servreq[server]
         new_server = cs.servers.create(server_name, image.id, flavor.id)
         new_serverid = new_server.id
-	print "Name:", new_server.name, "\nID:", new_server.id, "\nStatus:", new_server.status, "\nAdmin Password:", new_server.adminPass, 
+	print "Name:", new_server.name, "\nID:", new_server.id,\ 		"\nStatus:", new_server.status, "\nAdmin Password:", new_server.adminPass 
 	while not (new_server.networks)
 		time.sleep(10)
 		new_server = cs.servers.get(new_serverid)
