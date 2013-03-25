1c13f4d8e876aa013cc8a6586b6be5173d3087fc
root <root@pythonwork.(none)>
2013-03-25 00:04:25 +0000

    DONE

--- challenge1.py
+++ challenge1.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+#Challenge:  Write a script that builds three 512 MB Cloud Servers that following a similar naming convention. (ie., web1, web2, web3) and returns the IP and login credentials for each server. Use any image you want. Worth 1 point
+
+
 import pyrax
 import pyrax.exceptions as exc
 import os
