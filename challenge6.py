6ab0bab7b67a3e413fd18bb6c6f089716d932574
root <root@pythonwork.(none)>
2013-03-31 18:36:16 +0000

    Added a few comments, DONE.

--- challenge6.py
+++ challenge6.py
@@ -3,10 +3,11 @@
 '''
 Challenge 6: Write a script that creates a CDN-enabled container in Cloud Files. Worth 1 Point
 '''
-
+#Import the necessary libraries for this script.
 import pyrax
 import os
 
+#Use our own Auth code, because it is ugly otherwise
 creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 
 try:
@@ -16,14 +17,26 @@ except exc.AuthenticationFailed:
 print "Authenticated =", pyrax.identity.authenticated
 print
 
+#Connect to CloudFiles
 cf = pyrax.cloudfiles
 
-cont = cf.create_container("challenge6")
+#Get user input for our container name.
+
+cont_name = raw_input("What is your container named?\n")
+
+#Create the container, and create an object for it.
+cont = cf.create_container(cont_name)
+
+#Use the cont object to print our new containers data.
 print "Name:", cont.name
 print "# of objects:", cont.object_count
 
+#CDN Enable the container by passing the cont object through
+#its built in object "make_public" and set the ttl for 900(seconds).
 cont.make_public(ttl=900)
 
+#Pass the cont object through more attributes and print out 
+#the data about our CDN enabled object.
 print "cdn_enabled", cont.cdn_enabled
 print "cdn_ttl", cont.cdn_ttl
 print "cdn_log_retention", cont.cdn_log_retention
