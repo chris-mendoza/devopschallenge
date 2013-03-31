86bef70b151415db99567882c458ac35af40801b
root <root@pythonwork.(none)>
2013-03-31 18:09:52 +0000

    Added a few comments, and dynamic user input

--- challenge5.py
+++ challenge5.py
@@ -6,13 +6,12 @@ This instance should contain at least one database, and the database
 should have at least one user that can connect to it. Worth 1 Point.
 '''
 
-
-#!/usr/bin/env python
-
+#Import the libraries needed for our script.
 import pyrax
 import os
 import pyrax.exceptions as exc
 
+#Use our own exception for auth, because it's ugly otherwise
 creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 
 try:
@@ -22,16 +21,24 @@ except exc.AuthenticationFailed:
 print "Authenticated =", pyrax.identity.authenticated
 print
 
+#Set the cloud database library to a variable
 cdb = pyrax.cloud_databases
 
+db_name = raw_input("Name your database")
+user_name = raw_input("Database username?")
+passwd = raw_input("Password?")
+#Create a new object when we create out database.
 inst = cdb.create("first_instance", flavor=1, volume=2)
 
 print inst
 
+#Wait until our instance has created.
 pyrax.utils.wait_until(inst, "status", ['ACTIVE', 'ERROR'], interval=30, attempts=40, verbose=True)
 
-db = inst.create_database("db_name")
+#Create database inside of our instance.
+db = inst.create_database(db_name)
 print "DB:", db
 
-user = inst.create_user(name="chris", password="pizza", database_names=[db])
+#Create a user in our database
+user = inst.create_user(name=user_name, password=passwd, database_names=[db])
 print "User:", user
