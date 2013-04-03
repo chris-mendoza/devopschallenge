3acb25b227c9202febb66241f30fd1a5db78ad77
root <root@pythonwork.(none)>
2013-04-03 08:59:58 +0000

    Still working on this

--- challenge9.py
+++ challenge9.py
@@ -25,35 +25,41 @@ domain_name = raw_input("What is the FQDN we are going to use? \n")
 
 #Create an interactive interface to easily choose your image.
 
-i = 1
-while i:
+a = 1
+while a:
         inputid = raw_input("Type in Image Name:\n")
+        new_input = inputid.strip("use: ")
 
-        image = [img for img in cs.images.list()
+        search_image = [img for img in cs.images.list()
          if inputid in img.name]
 
-        print image
+        print search_image
 
-        if "use:" in inputid:
-                output = inputid.lstrip('use:')
-                print "Using image: ", image 
-                i = 0
+        if "use:" in inputid: 
+                saved_img = [img for img in cs.images.list()
+                             if new_input in img.name]
+                print "Using image: ", saved_img
+                a = 0
 
 
 #Create an interactive interface to easily choose your flavor.
+
 i = 1
 while i:
         flavorid = raw_input("Insert Flavor ID:\n")
+        new_flv = flavorid.strip("use:")
 
         flavor = [flv for flv in cs.flavors.list()
-                   if flavorid in flv.name][0]
+                   if flavorid in flv.name]
 
         print flavor
 
         if "use:" in flavorid:
-                flv_out = flavorid.lstrip('use:')
-                print "Using flavor: ", flavor
+                save_flv = [flv for flv in cs.flavors.list()
+                           if new_flv in flv.name]
+
+                print "Using flavor: ", new_flv
                 i = 0
 
 #Create the server 
-new_server = cs.servers.create(domain_name, image.id, flavor.id)
+new_server = cs.servers.create(domain_name, saved_img, save_flv)
